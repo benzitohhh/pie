b786d5a6988f7b12a3db4e96b36754320edf36ce
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2016-02-18 00:11:42 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+window.d3 = require('d3');
+
+var nv = require('nvd3');
+
+var DATA = [
+  {
+    "label": "One",
+    "value" : 29.765957771107
+  } ,
+  {
+    "label": "Two",
+    "value" : 0
+  } ,
+  {
+    "label": "Three",
+    "value" : 32.807804682612
+  } ,
+  {
+    "label": "Four",
+    "value" : 196.45946739256
+  } ,
+  {
+    "label": "Five",
+    "value" : 0.19434030906893
+  } ,
+  {
+    "label": "Six",
+    "value" : 98.079782601442
+  } ,
+  {
+    "label": "Seven",
+    "value" : 13.925743130903
+  } ,
+  {
+    "label": "Eight",
+    "value" : 5.1387322875705
+  }
+];
+
+
+nv.addGraph(function() {
+
+  var chart = nv.models.pieChart()
+        .x(function(d) { return d.label })
+        .y(function(d) { return d.value })
+        .showLabels(true)
+  ;
+
+  d3.select('#container svg')
+    .datum(DATA)
+    .call(chart)
+  ;
+
+  nv.utils.windowResize(chart.update);
+
+  return chart;
+});
