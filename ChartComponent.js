1a9644fc38c64566842d8d9965a9e87e3e0edb4d
Ben Immanuel <ben.immanuel@aistemos.com>
2016-02-18 14:03:51 +0000

    c3 react working ok

---
+++ ChartComponent.js
@@ -0,0 +1,82 @@
+/**
+ * React-C3 Chart
+ * Copyright 2015 - Cody Reichert <codyreichert@gmail.com>
+ * Adapted by Ben Immanuel - TODO: use Cody's version (but it is not currently in npm...)
+ */
+
+import c3    from 'c3';
+import React from 'react';
+
+const ChartC3 = React.createClass({
+    displayName: 'React-C3-Chart',
+
+    propTypes: {
+        data: React.PropTypes.object.isRequired,
+        element: React.PropTypes.string.isRequired,
+        type: React.PropTypes.string.isRequired,
+        options: React.PropTypes.object.isRequired
+    },
+
+    chart: null,
+
+    shouldComponentUpdate: function(nextProps) {
+        if(this.props.data.columns.length
+            !== nextProps.data.columns.length) { // shallow check
+            return true;
+        } else if(JSON.stringify(this.props.data.columns)
+                !== JSON.stringify(nextProps.data.columns)) { // deeper check
+            return  true;
+        }
+        return false;
+    },
+
+    componentDidMount: function() {
+        this._generateChart(
+            this.props.data.columns,
+            this.props.type,
+            this.props.element,
+            this.props.options
+        );
+    },
+
+    componentDidUpdate: function(prevProps) {
+        if(prevProps.data.columns !== this.props.data.columns) {
+            this._generateChart(
+                this.props.data.columns,
+                this.props.type,
+                this.props.element,
+                this.props.options
+            );
+        }
+    },
+
+    componentWillUnmount: function() {
+        this._destroyChart();
+    },
+
+    _generateChart: function(columns, type, element, options) {
+      let build = Object.assign({}, {
+        bindto: '#' + element,
+        data: {
+          columns: columns,
+          type: type
+        }
+      }, options);
+      this.chart = c3.generate(build);
+    },
+
+    _destroyChart: function() {
+        this.chart.destroy();
+    },
+
+    render: function() {
+        return (
+            <div className="c3 react-c3"
+                 id={this.props.element}
+                 style={this.props.styles}>
+            </div>
+        );
+    }
+});
+
+module.exports = ChartC3;
